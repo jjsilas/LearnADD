b021cc69daba63df7ba90fcc4bc337932e7e936d
Jherich <jjsilas@gmail.com>
2020-01-20 03:04:00 -0600

    add questions in the store.js file

---
+++ store.js
@@ -0,0 +1,61 @@
+const STORE = {
+    questions: [//1
+      {
+        question: "1. What is  another name for l'art du déplacement?",
+        options: [
+            “a. Parcours is the name originally used by the Yamakasi to describe what they did.”,
+            “b. Freerunning was a name coined in American to describe l''art du déplacement.”,
+            “c. Tracing is the official name for l''art du déplacement in Japan.”,
+            “d. Hardcore Parkour the name use in the series the Office is the most common name used by the Yamak.”
+        ],
+        answer: "Hyper Text Markup Language"
+      },
+      //2
+      {
+        question: "2. When was parcours initially created?",
+        options: [
+            “a. We don’t have a specific date but it was prior to 1954 with Raymond Belle’s return to France. “,
+            “b. We don’t have a specific date but it was prior to 1970 with Jackie Chan.”,
+            “c. We know for believe it was created by MTV in the early 2000’s for a TV game show.”,
+            “d. In the 80’s it was create by one of the constants on Ninja Warrior.”
+        ],
+        answer: "p"
+      },
+      //3
+      {
+        question: "Who are three of the original Yamakasi?",
+        options: [
+            “a. Three of the original Yamak are David Belle, Sébastien Foucan, Châu Belle Dinh.”,
+            “b.  Three of the original Yamak areWilliams Belle, Yann Hnautra, Taylor Lautner.”,
+            “c.  Three of the original Yamak areLaurent Piemontesi, Guylain N''Guba Boyeke, Paul Walker.”,
+            “d. Three of the original Yamak areMalik Diouf, Lance Armstrong, Châu Belle Dinh.”
+        ],
+        answer: "src"
+      },
+      //4
+      {
+        question: "What two movies feature movies related in the United States of America featured found members of the Yamakasi?", 
+        options: [
+            “a. Casino Royale and Brick Mansion featured founding member of the Yamakasi.”,
+            “b. Bad Boys and Pirates of the Caribbean featured founding member of the Yamakasi.”,
+            “c. Casino Royale and Fast and Furious featured founding member of the Yamakasi.”,
+            “a. Die Hard and Brick Mansion featured founding member of the Yamakasi.”
+        ],
+        answer: "a"
+      },
+      //5
+      {
+        question: "What does Yamakasi mean and what language is it from?",
+        options: [
+            “a. Yamakasi  means strong in one''s person, and is from the Nagal language of the Democratic Republic of the Congo.”,
+            “b. Yamakasi  means strong is the wise one, and is from the bahasa baku  language of Indonesia.”,
+            “c. Yamakasi  means those who run, and is from the Japanese language of the Japan.”,
+            “d. Yamakasi  means one who trains for running, and is from the Portuguese language of the Brazil.”
+        ],
+        answer: "< !-- This is HTML comment -->"
+      }
+      
+    ],
+    currentQuestion: 0,
+    score: 0
+  };
