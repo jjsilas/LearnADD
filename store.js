dd26ad107faeee77bac84d1e987abfe065cb5eda
Jherich <jjsilas@gmail.com>
2020-01-20 10:06:39 -0600

    changed answer place

--- store.js
+++ store.js
@@ -4,7 +4,7 @@ const STORE = {
         question: "1. What is  another name for l'art du déplacement?",
         options: [
             "Parcours is the name originally used by the Yamakasi to describe their movevment.",
-            "Freerunning was a name coined in American to describe l''art du déplacement.",
+            "Freerunning was a name coined in American to describe l'art du déplacement.",
             "Tracing is the official name for l''art du déplacement in Japan.",
             "Hardcore Parkour the name use in the series the Office is the most common name used by the Yamak."
         ],
@@ -14,20 +14,22 @@ const STORE = {
       {
         question: "2. When was parcours initially created?",
         options: [
-            "We don't have a specific date but it was prior to 1954 with Raymond Belle's return to France. ",
+
             "We don't have a specific date but it was prior to 1970 with Jackie Chan.",
+            "We don't have a specific date but it was prior to 1954 with Raymond Belle's return to France. ",
             "We know for believe it was created by MTV in the early 2000’s for a TV game show.",
             "In the 80’s it was create by one of the constants on Ninja Warrior."
         ],
-        answer: "We don'’t have a specific date but it was prior to 1954 with Raymond Belle'’s return to France."
+        answer: "We don't have a specific date but it was prior to 1954 with Raymond Belle'’s return to France."
       },
       //3
       {
         question: "Who are three of the original Yamakasi?",
         options: [
-            "Three of the original Yamak are David Belle, Sébastien Foucan, Châu Belle Dinh.",
+            
             "Three of the original Yamak areWilliams Belle, Yann Hnautra, Taylor Lautner.",
             "Three of the original Yamak areLaurent Piemontesi, Guylain N''Guba Boyeke, Paul Walker.",
+            "Three of the original Yamak are David Belle, Sébastien Foucan, Châu Belle Dinh.",
             "Three of the original Yamak areMalik Diouf, Lance Armstrong, Châu Belle Dinh."
         ],
         answer: "Three of the original Yamak are David Belle, Sébastien Foucan, Châu Belle Dinh."
@@ -36,10 +38,10 @@ const STORE = {
       {
         question: "What two movies feature movies related in the United States of America featured found members of the Yamakasi?", 
         options: [
-            "Casino Royale and Brick Mansion featured founding member of the Yamakasi.",
             "Bad Boys and Pirates of the Caribbean featured founding member of the Yamakasi.",
             "Casino Royale and Fast and Furious featured founding member of the Yamakasi.",
-            "Die Hard and Brick Mansion featured founding member of the Yamakasi."
+            "Die Hard and Brick Mansion featured founding member of the Yamakasi.",
+            "Casino Royale and Brick Mansion featured founding member of the Yamakasi."
         ],
         answer: "Casino Royale and Brick Mansion featured founding member of the Yamakasi."
       },
@@ -47,7 +49,7 @@ const STORE = {
       {
         question: "What does Yamakasi mean and what language is it from?",
         options: [
-            "Yamakasi means strong in one''s person, and is from the Nagal language of the Democratic Republic of the Congo.",
+            "Yamakasi means strong in one's person, and is from the Nagal language of the Democratic Republic of the Congo.",
             "Yamakasi means strong is the wise one, and is from the bahasa baku  language of Indonesia.",
             "Yamakasi means those who run, and is from the Japanese language of the Japan.",
             "Yamakasi means one who trains for running, and is from the Portuguese language of the Brazil."
